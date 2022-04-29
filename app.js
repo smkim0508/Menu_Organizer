5af160686a6b377643e4e84e07b8817bb68f605f
Sungmin Kim <chefminkim58@gmail.com>
2022-04-29 08:55:53 -0400

    added dynamic web functions

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const logger = require ("morgan");
 const app = express();
 const port = 8080;
 
+//configure express to use EJS
+app.set("views", __dirname + "/views")
+app.set("view engine", "ejs");
+
+const db = require('./db/db_connection')
+
 // define middleware that logs all incoming requests
 app.use(logger("dev"));
 
@@ -12,9 +18,43 @@ app.use(express.static(__dirname + '/public'));
 
 // define a route for the default home page
 app.get( "/", ( req, res ) => {
-
-    res.sendFile( __dirname + "/views/index.html" );
+    res.render('index');
 } );
+
+const read_orders_all_sql = `
+    SELECT
+        id, item, quantity, requests
+    FROM
+        orders
+`
+
+app.get("/menu", (req, res ) => {
+    db.execute(read_orders_all_sql, (error, results) => {
+        if (error)
+            res.status(500).send(error); //internal server error
+        else
+            res.send(results);
+    })
+} );
+
+const read_orders_item_sql = `
+    SELECT
+        id, item, quantity, requests
+    FROM
+        orders
+    WHERE
+    id = ?
+`
+//define a route for the item detail page
+app.get( "/menu/item/:id", (req, res ) => {
+    db.execute(read_orders_item_sql, [req.params.id], (error, results) => {
+        if(error)
+            res.status(500).send(error); //internal service error
+        else if (results.length == 0)
+            res.status(404).send(`No item found with id = ${req.params.id}`) //no page found error
+        else    res.send(results[0]);
+    })
+}); 
 
 // define a route for the stuff inventory page
 app.get( "/menu/item", ( req, res ) => {
