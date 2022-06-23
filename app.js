ed8f30eb86a094d4f90bc3c248a78d3f5b40bfed
Sungmin Kim <chefminkim58@gmail.com>
2022-06-22 20:23:39 -0400

    redirect user based on admin permission

--- app.js
+++ app.js
@@ -108,34 +108,51 @@ const check_user_match_sql = `
         email = ?
 `
 
+const check_admin_permission_sql =`
+    SELECT
+        isAdmin
+    FROM
+        users
+    WHERE
+        email = ?
+`
 //renders the menu ordering page and checks if logged in user is within the admin db
 
 app.get("/menu", requiresAuth(), (req, res ) => {
-    db.execute(read_combined_all_sql, [req.oidc.user.email], (error, results) => {
+    db.execute(check_admin_permission_sql, [req.oidc.user.email], (error, results) => {
         if (error)
             res.status(500).send(error);
-        else
-            db.execute(read_sum_sql, [req.oidc.user.email], (error, results2) => {
+        else if (results[0].isAdmin == 1) {
+            res.redirect("/admin")
+        }
+        else {
+            db.execute(read_combined_all_sql, [req.oidc.user.email], (error, results1) => {
                 if (error)
-                    res.status(500).send(error); //internal server error
-                else {
-                    res.render('menu', { inventory : results, username : req.oidc.user.name, sum : results2[0].sum });
-                    // console.log("render successful")
-                    db.execute(check_user_match_sql, [req.oidc.user.email], (error, results3) => {
+                    res.status(500).send(error);
+                else
+                    db.execute(read_sum_sql, [req.oidc.user.email], (error, results2) => {
                         if (error)
-                            res.status(500).send(error);
-                        else if (results3.length == 0){
-                            console.log("added user to db")
-                            db.execute(add_user_sql, [req.oidc.user.email], (error, results4) => {
-                                if (error)
-                                    res.status(500).send(error)
-                            })
-                        }
+                            res.status(500).send(error); //internal server error
                         else {
-                            console.log("user exists in db")
-                        }
-                    })} 
+                            res.render('menu', { inventory : results1, username : req.oidc.user.name, sum : results2[0].sum });
+                            // console.log("render successful")
+                            db.execute(check_user_match_sql, [req.oidc.user.email], (error, results3) => {
+                                if (error)
+                                    res.status(500).send(error);
+                                else if (results3.length == 0){
+                                    console.log("added user to db")
+                                    db.execute(add_user_sql, [req.oidc.user.email], (error, results4) => {
+                                        if (error)
+                                            res.status(500).send(error)
+                                    })
+                                }
+                                else {
+                                    console.log("user exists in db")
+                                }
+                            })} 
+                    })
             })
+        }
     })
 })
 
