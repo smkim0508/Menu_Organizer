19cecd8d31775e329080ee3b1236c81be5700368
Sungmin Kim <chefminkim58@gmail.com>
2022-09-20 13:28:28 -0400

    app.js error fix attempt 4

--- app.js
+++ app.js
@@ -155,7 +155,7 @@ const add_user_sql=`
         (?, 0)
 `
 
-// a check to make sure user is correctly identified
+// a check for whether the user exists in db
 const check_user_match_sql = `
     SELECT
         email
@@ -168,48 +168,43 @@ const check_user_match_sql = `
 //renders the menu ordering page and checks if logged in user is within the admin db
 
 app.get("/menu", requiresAuth(), (req, res ) => {
-    db.execute(check_admin_permission_sql, [req.oidc.user.email], (error, results) => {
+    db.execute(check_user_match_sql, [req.oidc.user.email], (error, results) => {
         if (error)
             res.status(500).send(error);
-        else if (results[0].isAdmin == 1) {
-            res.redirect("/admin")
-            console.log("user is an admin");
-            // console.log(results);
-            // console.log(results[0]);
-            // console.log(results.isAdmin);
-            console.log(results[0].isAdmin);
-        }
         else {
-            console.log("user is a user");
-            // console.log(results);
-            // console.log(results[0]);
-            // console.log(results.isAdmin);
-            console.log(results[0].isAdmin);
-            db.execute(read_combined_all_sql, [req.oidc.user.email], (error, results1) => {
+            if (results.length == 0) {
+                db.execute(add_user_sql, [req.oidc.user.email], (error) => {
+                    if (error)
+                        res.status(500).send(error);
+                })
+                console.log("added user to db");
+            }
+            else {
+                console.log("user exists in db");
+            }
+            db.execute(check_admin_permission_sql, [req.oidc.user.email], (error, results2) => {
                 if (error)
                     res.status(500).send(error);
-                else
-                    db.execute(read_sum_sql, [req.oidc.user.email], (error, results2) => {
+                else if (results2[0].isAdmin == 1) {
+                    res.redirect("/admin")
+                    console.log("user is an admin");
+                }
+                else {
+                    console.log("user is a user");
+                    db.execute(read_combined_all_sql, [req.oidc.user.email], (error, results3) => {
                         if (error)
-                            res.status(500).send(error); //internal server error
-                        else {
-                            res.render('menu', { inventory : results1, username : req.oidc.user.name, sum : results2[0].sum });
-                            // console.log("render successful")
-                            db.execute(check_user_match_sql, [req.oidc.user.email], (error, results3) => {
+                            res.status(500).send(error);
+                        else
+                            db.execute(read_sum_sql, [req.oidc.user.email], (error, results4) => {
                                 if (error)
-                                    res.status(500).send(error);
-                                else if (results3.length == 0){
-                                    console.log("added user to db")
-                                    db.execute(add_user_sql, [req.oidc.user.email], (error) => {
-                                        if (error)
-                                            res.status(500).send(error);
-                                    })
-                                }
+                                    res.status(500).send(error); //internal server error
                                 else {
-                                    console.log("user exists in db")
-                                }
-                            })} 
+                                    res.render('menu', { inventory : results3, username : req.oidc.user.name, sum : results4[0].sum });
+                                    // console.log("render successful")
+                                    } 
+                            })
                     })
+                }
             })
         }
     })
